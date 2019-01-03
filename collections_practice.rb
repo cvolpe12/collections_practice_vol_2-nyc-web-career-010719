cc902e2230a822d8a22000f2a1b57c29e3880413
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-03 20:13:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
 # your code goes here
+def begins_with_r(array)
+  array.select |words|
+    words == "r"
+end
+
+def contain_a(array)
+
+end
+
+def first_wa(array)
+
+end
+
+def remove_non_strings(array)
+
+end
+
+def count_elements(array)
+
+end
+
+def merge_data(array1, array2)
+  array1.concat array2
+end
+
+def find_cool(array)
+
+end
+
+def organize_schools(array)
+
+end
